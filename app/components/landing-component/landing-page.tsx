5625a13d7d00ea2629a4ff4de8e3014d100fc0ca
Akshay-Mario <akshay1997qw@gmail.com>
2024-07-19 00:03:06 +0530

    services section

--- app/components/landing-component/landing-page.tsx
+++ app/components/landing-component/landing-page.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Container, Group, Image, Paper, Title } from "@mantine/core";
+import { Anchor, Container, Grid, Group, Image, Paper, SimpleGrid, Text, Title } from "@mantine/core";
 import styles from './landing-page.module.scss'
 import { useEffect, useState } from "react";
 
@@ -19,12 +19,56 @@ export default function LandingPage() {
         };
     }, []);
 
+
+    const gridData = [
+        {
+            svg:
+                'assets/images/psLogo.svg',
+            title: 'PS5 GAMING',
+            subText: 'Dive into the cutting-edge world of PS5 gaming. Enjoy breathtaking visuals and lightning-fast performance in our state-of-the-art lounge.',
+        },
+        {
+            svg:
+                'assets/images/retroLogo.svg',
+            title: 'RETRO GAMES',
+            subText: 'Take a trip down memory lane with our collection of retro games. From classic arcade machines to beloved console titles, we have it all.',
+        },
+
+        {
+            svg: 'assets/images/stayTuned.svg',
+            title: 'STAY TUNED',
+            subText: 'Stay tuned for exciting new services coming your way. We’re always looking to expand and enhance your gaming experience.',
+        },
+
+    ];
+
+
+    const gridDetails = gridData.map((item, index) => {
+        // console.log("index :",index)
+        return (
+            <div key={index} className={styles.gridDiv}>
+                <div className={styles.imageTextDiv}>
+                    <Image
+                        src={item.svg}
+                        alt="Descriptive alt text"
+                        width={32}
+                        height={32}
+                    />
+                    <Title style={{ textAlign: 'center' }} order={3}>{item.title}</Title>
+                </div>
+                <Text className={styles.textDescription} >{item.subText}</Text>
+            </div>)
+    }
+    );
+
+
+
     return (
         <>
             <Paper
                 className={`${styles.titleNav}`}>
                 <Image
-                    width={130}
+                    width={100}
                     src={'assets/images/RespawnGaming-logo.svg'}
                     alt={'Respawn Gaming Logo'}
                 />
@@ -51,7 +95,12 @@ export default function LandingPage() {
                 </Group>
 
             </Paper>
-            <Container className={styles.section1} >
+            <Container className={styles.section1} 
+            style={{ 
+                zIndex: 0,
+                transform: `translateY(${offsetY * 0.3}px)` 
+            }}
+            >
                 <div style={{ display: 'flex', flexDirection: 'column' }}>
                     <Title order={1}>PLAY,</Title>
                     <Title order={1}>RELAX,</Title>
@@ -65,7 +114,7 @@ export default function LandingPage() {
                 <Anchor href="https://www.google.com/maps/dir//Tata+Consultancy+Services,+Infopark+E,+Kakkanad,+Kerala" target="_blank" >
                     <span>
                         <Image
-                        color={'white'}
+                            color={'white'}
                             width={10}
                             src={'assets/images/myLocation.svg'}
                         />
@@ -73,43 +122,33 @@ export default function LandingPage() {
                 </Anchor>
                 <Image className={styles.ps5Main}
                     width={150}
-                    style={{zIndex: 2, transform: `translateY(-${offsetY * 0.2}px)` }} 
+                    style={{ zIndex: 2, 
+                        // transform: `translateY(-${offsetY * 0.2}px)` 
+                    }}
                     src={'assets/images/controller1.svg'}
                     alt={'Respawn Gaming Logo'}
                 />
                 <Image className={styles.ps5Main2}
                     width={70}
-                    style={{ zIndex: 0, transform: `translateY(${offsetY * 0.07}px)` }} 
+                    style={{ zIndex: 0, 
+                        // transform: `translateY(${offsetY * 0.07}px)` 
+                    }}
                     src={'assets/images/controller2.svg'}
                     alt={'Respawn Gaming Logo'}
                 />
             </Container>
-            <Container className={styles.section2} >
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <Title order={1}>PLAY,</Title>
-                    <Title order={1}>RELAX,</Title>
-                    <Title style={{ marginBottom: '0.2%' }} order={1}>RESPAWN.</Title>
-                    <Title order={6}>JOIN US AT <span> <Image
-                        width={310}
-                        src={'assets/images/respawnText.svg'}
-                        alt={'Respawn Gaming Logo'}
-                    /> .</span></Title>
+            <Container className={styles.section2}
+            style={{ 
+                zIndex: 10,
+                transform: `translateY(${offsetY * 0}px)` 
+            }} >
+                <Title order={1}>OUR SERVICES</Title>
+                <div className={styles.mainGrid}>
+                    {gridDetails}
                 </div>
-                <Anchor href="https://www.google.com/maps/dir//Tata+Consultancy+Services,+Infopark+E,+Kakkanad,+Kerala" target="_blank" >
-                    <span>
-                        <Image
-                        color={'white'}
-                            width={10}
-                            src={'assets/images/myLocation.svg'}
-                        />
-                    </span>Locate us on map
-                </Anchor>
-                {/* <Image className={styles.ps5Main}
-                    width={150}
-                    src={'assets/images/controller1.svg'}
-                    alt={'Respawn Gaming Logo'}
-                /> */}
+
             </Container>
+            
 
         </>
     )
