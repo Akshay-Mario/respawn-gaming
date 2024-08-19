681d3ac9bd695fbb16745ff1f93a4c91c8b547a5
Akshay-Mario <akshay1997qw@gmail.com>
2024-08-19 09:44:57 +0530

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.png" type="image/png"></link>
         <link rel="canonical" href="https://www.respawngaming.in/" />
       </head>
       <body className={inter.className}>{children}</body>
